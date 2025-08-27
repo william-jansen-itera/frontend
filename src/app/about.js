3147adecf86ebbd5bcd676a4fdbb9cbfd222c46a
William <william@jansen.net>
2025-08-27 15:04:48 +0200

    auth button

---
+++ src/app/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is a new page in your Next.js Static Web App.</p>
+    </div>
+  );
+}
---
+++ src/app/useAuth.js
@@ -0,0 +1,25 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export function useAuth() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    async function getUser() {
+      const res = await fetch("/.auth/me");
+      const data = await res.json();
+      setUser(data.clientPrincipal || null);
+    }
+    getUser();
+  }, []);
+
+  function signIn() {
+    window.location.href = "/.auth/login";
+  }
+
+  function signOut() {
+    window.location.href = "/.auth/logout";
+  }
+
+  return { user, signIn, signOut };
+}
