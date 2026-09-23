82aa47278a4a97affbc2d0e02bd129c289d488b2
William <william@jansen.net>
2026-09-23 11:00:54 +0200

    ss

--- azure-sql/page-visit-counters.sql
+++ azure-sql/page-visit-counters.sql
@@ -12,11 +12,27 @@ BEGIN
     first_visited_at DATETIME2(7) NOT NULL CONSTRAINT DF_page_visit_counters_first_visited_at DEFAULT (SYSUTCDATETIME()),
     latest_visited_at DATETIME2(7) NOT NULL CONSTRAINT DF_page_visit_counters_latest_visited_at DEFAULT (SYSUTCDATETIME()),
     updated_at DATETIME2(7) NOT NULL CONSTRAINT DF_page_visit_counters_updated_at DEFAULT (SYSUTCDATETIME()),
-    CONSTRAINT CK_page_visit_counters_page_path CHECK (page_path IN (N'/', N'/about')),
     CONSTRAINT CK_page_visit_counters_referrer_host CHECK (LEN(referrer_host) > 0),
     CONSTRAINT CK_page_visit_counters_device_class CHECK (LEN(device_class) > 0),
     CONSTRAINT CK_page_visit_counters_browser_family CHECK (LEN(browser_family) > 0)
   );
+END;
+
+IF OBJECT_ID(N'dbo.page_visit_counters', N'U') IS NOT NULL
+BEGIN
+  IF EXISTS (
+    SELECT 1
+    FROM sys.check_constraints
+    WHERE parent_object_id = OBJECT_ID(N'dbo.page_visit_counters')
+      AND name = N'CK_page_visit_counters_page_path'
+  )
+  BEGIN
+    ALTER TABLE dbo.page_visit_counters DROP CONSTRAINT CK_page_visit_counters_page_path;
+  END;
+
+  ALTER TABLE dbo.page_visit_counters
+    ADD CONSTRAINT CK_page_visit_counters_page_path
+    CHECK (page_path IN (N'/', N'/about', N'/contact'));
 END;
 
 IF NOT EXISTS (
